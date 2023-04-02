f03b8686dd034182c402fb568a7d90fbd61ecb80
kerfalla <Kerfalla.Cisse@etu.unige.ch>
2023-04-02 19:34:16 +0200

    chore: clean repo

--- src/test/java/org/gs/tutorial/quarkusmaven/NativeStartQuarkusIT.java
+++
@@ -1,9 +0,0 @@
-package org.gs.tutorial.quarkusmaven;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeStartQuarkusIT extends StartQuarkusTest {
-
-    // Execute the same tests but in native mode.
-}
