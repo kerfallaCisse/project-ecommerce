76da6203ae30cb618176776a37cb1dd51daef1f8
Stacy Demange <stacy.demange@etu.unige.ch>
2023-04-27 16:46:45 +0200

    fix(ui) : display

--- web_ui/src/app/app-routing.module.ts
+++ web_ui/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import{SacPageComponent} from './sac-page/sac-page.component';
 import { LoginComponent } from './login/login.component'
-import { PrecisionComponent } from './login/precision/precision.component' 
+import { PrecisionComponent } from './login/precision/precision.component'
 
 
 
@@ -12,7 +12,7 @@ const routes: Routes = [
 {path:'search/:searchTerm',component:HomeComponent},
 {path:'tag/:tag',component:HomeComponent},
 {path: 'food/:id',component:SacPageComponent},
-{ path: 'login', component: LoginComponent },
+{path: 'login', component: LoginComponent },
 {path: 'precision', component: PrecisionComponent},
 { path: 'login', component: LoginComponent, children: [
   { path: 'precision', component: PrecisionComponent }
