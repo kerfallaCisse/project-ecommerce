87791738d64b5246e636f5d0f23388a010d99b5c
Keertika Yogendra <keertika.yogendra@etu.unige.ch>
2023-04-28 14:52:27 +0000

    fix(custom): remove test

--- customization/src/test/java/api/model/CustomizationResourceTest.java
+++ customization/src/test/java/api/model/CustomizationResourceTest.java
@@ -9,13 +9,5 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class CustomizationResourceTest {
 
-    @Test
-    public void testCustomizationEndpoint() {
-        given()
-          .when().get("customization")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from customization endpoint"));
-    }
-
+ 
 }
