2751e179867a97765b4690d05234710410834074
vboxuser <vboxuser@ProjetInfo.myguest.virtualbox.org>
2023-05-03 23:16:16 +0200

    feature: customization service beta

---
+++ customization/src/main/java/api/model/SmallModel.java
@@ -0,0 +1,24 @@
+package api.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+@Table(name="SmallModel")
+public class SmallModel extends PanacheEntity{
+
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(name="bag_name")
+    public String bag_name;
+
+    @Column(name="cloudinary_url")
+    public String cloudinary_url;
+}
