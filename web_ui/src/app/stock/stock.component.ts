1ff1fb66aebd3ea03a3072620b04eab4913bdb07
Charles MARTIN <charles.martin@etu.unige.ch>
2023-05-04 14:30:29 +0200

    stock page 2.1

--- web_ui/src/app/stock/stock.component.ts
+++ web_ui/src/app/stock/stock.component.ts
@@ -68,10 +68,13 @@ ngOnInit(): void {
     }
 
     func_for_modifiquantit(color_bag: string,color_pocket: string,quantiti: number,model: string ,add: boolean) {
-
+      
       for (var i = 0; i < this.tab.length; i++) {
+        console.log(color_bag,color_pocket,model)
+        console.log(this.tab[i])
         console.log("on rentre dans la modif")
         if (this.tab[i].color_bag_name ===  color_bag && this.tab[i].color_pocket_name === color_pocket && this.tab[i].modelType === model) {
+          console.log("TROUVER")
           if(add == true){
           this.tab[i].quantity = this.tab[i].quantity + +quantiti;
          
@@ -90,11 +93,12 @@ ngOnInit(): void {
 
     onSubmit_add() {
       this.func_for_modifiquantit(this.selectedColor_bag,this.selectedColor_pocket,this.numberr,this.model_bag,true)
-      
+      this.func_resume_stock()
     }
 
     onSubmit_remove() {
       this.func_for_modifiquantit(this.selectedColor_bag_2,this.selectedColor_pocket_2,this.numberr,this.model_bag_2,false)
+      this.func_resume_stock()
     }
     
   constructor(private stockService: StockService) {}
