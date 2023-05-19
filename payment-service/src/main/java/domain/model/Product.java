2bc04dc0f2bd9191f10cf409e7d05f85c3938a66
Aylin Tekkoyun <aylin.tekkoyun@etu.unige.ch>
2023-05-19 16:04:00 +0200

    trying new version jdk

---
+++ payment-service/src/main/java/domain/model/Product.java
@@ -0,0 +1,9 @@
+package domain.model;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    public String prodId;
+    public int quantity;
+}
--- payment-service/src/test/java/api/model/PaymentResourceTest.java
+++ payment-service/src/test/java/api/model/PaymentResourceTest.java
@@ -1,7 +1,6 @@
 package api.model;
 
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -9,12 +8,4 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class PaymentResourceTest {
 
-    @Test
-    public void testPaymentEndpoint() {
-        given()
-          .when().get("/payment")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from payment endpoint"));
-    }
 }
