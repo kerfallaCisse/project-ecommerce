53c54fc719e719eb81e224851425c73dd93c7946
vboxuser <vboxuser@ProjetInfo.myguest.virtualbox.org>
2023-06-10 19:34:23 +0200

    fix: logo upload

--- customization/src/main/java/api/model/CustomizationFormData.java
+++ customization/src/main/java/api/model/CustomizationFormData.java
@@ -9,4 +9,19 @@ public class CustomizationFormData {
     @FormParam("file")
     public File file;
 
+    @FormParam("modelType")
+    public String modelType;
+    
+    @FormParam("bagColor")
+    public String bagColor;
+    
+    @FormParam("pocketColor")
+    public String pocketColor;
+    
+    @FormParam("email")
+    public String email;
+    
+    @FormParam("quantity")
+    public String quantity;
+
 }
--- customization/src/main/java/api/model/CustomizationResource.java
+++ customization/src/main/java/api/model/CustomizationResource.java
@@ -17,15 +17,13 @@ import javax.imageio.ImageIO;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
-import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
 
 import com.cloudinary.Cloudinary;
@@ -34,30 +32,40 @@ import com.cloudinary.utils.ObjectUtils;
 @Path("/customization")
 public class CustomizationResource {
 
-     
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public JsonObject getBag(@QueryParam("modelType") String modelType, @QueryParam("bagColor") String bagColor,
-            @QueryParam("pocketColor") String pocketColor) {
-        JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
+    @ConfigProperty(name = "cloudinary.cloud_name")
+    String cloud_name;
 
-        if (modelType == null) {
-            modelType = "largeModel";
-        }
-        if (bagColor == null) {
-            bagColor = "Black";
-        }
-        if (pocketColor == null) {
-            pocketColor = "Black";
-        }
-        if ("largeModel".equals(modelType) || "smallModel".equals(modelType)) {
-            LargeModel largeModel = LargeModel.find("bag_name", bagColor + pocketColor).firstResult();
+    @ConfigProperty(name = "cloudinary.api_key")
+    String api_key;
+
+    @ConfigProperty(name = "cloudinary.api_secret")
+    String api_secret;
+
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject getBag(@MultipartForm CustomizationFormData bagModel) {
+        JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
+        
+        if ("largeModel".equals(bagModel.modelType) || "smallModel".equals(bagModel.modelType)) {
+            LargeModel largeModel = LargeModel.find("bag_name", bagModel.bagColor + bagModel.pocketColor).firstResult();
             if (largeModel == null) {
                 return jsonObjectBuilder.add("error", "bag not found").build();
             } else {
-                jsonObjectBuilder
-                    .add("id", largeModel.id)
-                    .add("cloudinary_url", largeModel.cloudinary_url);
+                try {
+                    File bagWithLogo = mergeImages(largeModel.cloudinary_url, bagModel.file);
+                    String cloudinary_bagWithLogo = upload_image(bagWithLogo);
+                    
+                    jsonObjectBuilder
+                    .add("email", bagModel.email)
+                    .add("modelType", bagModel.modelType)
+                    .add("bagColor", bagModel.bagColor)
+                    .add("pocketColor", bagModel.pocketColor)
+                    .add("quantity", bagModel.quantity)
+                    .add("cloudinary_url", cloudinary_bagWithLogo);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
@@ -82,71 +90,38 @@ public class CustomizationResource {
 
     }
 
-    // IMPORTANT change manually image path and model.bag_name
-    @Path("/upload")
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    @Transactional
-    public String upload_image() {
+    public String upload_image(File bagWithLogo) {
         Map<String, String> config = new HashMap<String, String>();
-        // Ask Denis for credits
-        config.put("cloud_name", "");
-        config.put("api_key", "");
-        config.put("api_secret", "");
+        config.put("cloud_name", cloud_name);
+        config.put("api_key", api_key);
+        config.put("api_secret", api_secret);
         Cloudinary cloudinary = new Cloudinary(config);
 
         try {
             // Upload
-            Map<String, Object> uploadResult = cloudinary.uploader().upload("BlackBlack.jpeg", ObjectUtils.emptyMap()); // Change
-                                                                                                                        // URL
-
+            Map<String, Object> uploadResult = cloudinary.uploader().upload(bagWithLogo, ObjectUtils.emptyMap()); // Change URL
             // Get the public URL
             String publicUrl = cloudinary.url().generate(uploadResult.get("public_id").toString());
-            System.out.println(publicUrl);
 
-            // Add URL to database
+            /* Add URL to database
             LargeModel model = new LargeModel();
             model.bag_name = ""; // Set the bag name here. Ex BlackBlack
             model.cloudinary_url = publicUrl;
             model.persist(); // Save the entity to the database
-
+            */
             return publicUrl;
         } catch (IOException exception) {
             System.out.println(exception.getMessage());
             return "Error: " + exception.getMessage();
         }
     }
-    
 
-    // Upload logo and put on a bag, upload image on cloudinary
-    /*@Path("/add_logo")
-    @POST
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    @Produces(MediaType.APPLICATION_JSON)
-    @Transactional
-    public JsonObject addLogo(@MultipartForm CustomizationFormData formData) throws IOException {
-        JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
-
-        BufferedImage bagImage = CustomizationFormData.bagImage;
-        BufferedImage newLogo = CustomizationFormData.newLogo;
-
-        Graphics2D g = (Graphics2D) bagImage.createGraphics();
-        g.drawImage(newLogo, (bagImage.getWidth() - newLogo.getWidth()) / 2, (bagImage.getHeight() - newLogo.getHeight()) / 2, null);
-        // Replace by db upload 
-        ImageIO.write(bagImage, "jpg", new File("bagWithLogo.jpg"));
-
-        return jsonObjectBuilder.add("bag", "cloudinary url").build();
-    }*/
-
-    @Path("/add_logo")
-    @POST
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public String mergeImages(@MultipartForm CustomizationFormData imageData) throws IOException {
-        byte[] formData = Files.readAllBytes(imageData.file.toPath());
+    public File mergeImages(String imageUrl, File logoFile) throws IOException {
+        byte[] formData = Files.readAllBytes(logoFile.toPath());
         ByteArrayInputStream bInputStream = new ByteArrayInputStream(formData);
         BufferedImage logo = ImageIO.read(bInputStream); // Uploaded logo 150x150 px
 
-        URL url = new URL("http://res.cloudinary.com/dqvvvce88/image/upload/wz1dbmyo22ohwuug3nbi");
+        URL url = new URL(imageUrl);
         InputStream inputStream = url.openStream();
         File file = File.createTempFile("temp", null);
         FileOutputStream outputStream = new FileOutputStream(file);
@@ -174,9 +149,10 @@ public class CustomizationResource {
         g2d.dispose();
 
         // Save the merged image to a file (optional)
-        ImageIO.write(bagImage, "png", new File("logo2.png"));
+        File outputFile = new File("logo2.png");
+        ImageIO.write(bagImage, "png", outputFile);
 
-        return "ok";
+        return outputFile;
     }
     
 }
