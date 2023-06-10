f41b1db03349cd6f1448dc2ce08b2c4e078c63a3
Aylin Tekkoyun <aylin.tekkoyun@etu.unige.ch>
2023-06-10 11:18:15 +0200

    path stacy

--- payment-service/src/main/java/domain/service/PaymentServiceImpl.java
+++ payment-service/src/main/java/domain/service/PaymentServiceImpl.java
@@ -31,6 +31,9 @@ public class PaymentServiceImpl implements PaymentService{
     public  JsonObject createCheckoutSession(Basket basket) throws StripeException{
         Stripe.apiKey = apiKey;
 
+        // String url = uriInfo.getRequestUri().toString();
+        // System.out.println(url);
+
         List<SessionCreateParams.LineItem> lineItems = new ArrayList<>();
         for (ProductBasket product : basket.getProducts()) {
             int quantity = product.getQuantity();
@@ -58,8 +61,10 @@ public class PaymentServiceImpl implements PaymentService{
             SessionCreateParams params = SessionCreateParams.builder()
             .setMode(SessionCreateParams.Mode.PAYMENT)
             .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
-            .setSuccessUrl("https://pinfo3.unige.ch/success")
-            .setCancelUrl("https://pinfo3.unige.ch/fail")
+            // .setSuccessUrl("https://pinfo3.unige.ch/success")
+            // .setCancelUrl("https://pinfo3.unige.ch/fail")
+            .setSuccessUrl("http://localhost:4200/success")
+            .setCancelUrl("http://localhost:4200/fail")
             .setCurrency("chf")
             .addAllLineItem(lineItems)
             .build();
