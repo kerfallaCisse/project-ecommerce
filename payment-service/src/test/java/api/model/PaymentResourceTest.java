c9ba8e353e8f111123ef35b8d79a9189c29ca7f5
Aylin Tekkoyun <aylin.tekkoyun@etu.unige.ch>
2023-06-15 10:05:58 +0200

    database Test(payment-service)

--- payment-service/src/test/java/api/model/PaymentResourceTest.java
+++ payment-service/src/test/java/api/model/PaymentResourceTest.java
@@ -2,10 +2,10 @@ package api.model;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+// import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+// import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import api.rest.PaymentRestService;
@@ -15,6 +15,7 @@ import domain.model.Profit;
 import domain.service.PaymentServiceImpl;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,14 +30,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.hamcrest.CoreMatchers.equalTo;
+// import static org.mockito.ArgumentMatchers.any;
+// import static org.mockito.Mockito.*;
+// import static org.hamcrest.CoreMatchers.equalTo;
 
 
 @QuarkusTest
 @TestHTTPEndpoint(PaymentRestService.class)
+@TestProfile(ProfitTestProfile.class)
 @Tag("integration")
 public class PaymentResourceTest {
 
@@ -108,7 +109,6 @@ public class PaymentResourceTest {
 /* Test for Basket Model */
     @Test
     void testBasket() {
-
         List<ProductBasket> productBaskets = new ArrayList<>();
 
         ProductBasket productBasket1 = new ProductBasket();
@@ -128,8 +128,7 @@ public class PaymentResourceTest {
     }
 
     @Test
-    void testEqualsBasket() {
-        // Create two Basket objects with the same values
+    void testEqualsBasket(){
         Basket basket1 = new Basket();
         List<ProductBasket> products1 = new ArrayList<>();
         ProductBasket product1 = new ProductBasket();
@@ -146,13 +145,11 @@ public class PaymentResourceTest {
         products2.add(product2);
         basket2.setProducts(products2);
 
-        // Assert that the two objects are equal
         assertEquals(basket1, basket2);
     }
 
     @Test
     void testToStringBasket() {
-        // Create a Basket object
         Basket basket = new Basket();
         List<ProductBasket> products = new ArrayList<>();
         ProductBasket product = new ProductBasket();
@@ -161,16 +158,13 @@ public class PaymentResourceTest {
         products.add(product);
         basket.setProducts(products);
 
-        // Get the string representation of the object
         String expectedToString = "Basket(products=[ProductBasket(prodId=ABC123, quantity=2)])";
 
-        // Assert that the toString() method returns the expected string
         assertEquals(expectedToString, basket.toString());
     }
 
     @Test
     void testHashCodeBasket() {
-        // Create two Basket objects with the same values
         Basket basket1 = new Basket();
         List<ProductBasket> products1 = new ArrayList<>();
         ProductBasket product1 = new ProductBasket();
@@ -187,7 +181,6 @@ public class PaymentResourceTest {
         products2.add(product2);
         basket2.setProducts(products2);
 
-        // Assert that the hash codes are equal
         assertEquals(basket1.hashCode(), basket2.hashCode());
     }
 
@@ -258,20 +251,19 @@ public class PaymentResourceTest {
     }
 
 /* Test PaymentServiceImpl */
-
-
     @Test
     void testPaymentServiceImpl(){
         PaymentServiceImpl paymentService = new PaymentServiceImpl();        
         assertNotNull(paymentService);
     }
 
-    // @ConfigProperty(name = "stripe.api.key") 
-    // String apiKey;
-    
-    // @Test
-    // void testCreateCheckoutSession() throws StripeException {
+    // @ConfigProperty(name = "stripe.api.key.test") 
+    // String apiKeyTest;
 
+    // @Test
+    // void testCreateCheckoutSession() throws StripeException{
+    //     Stripe.apiKey = apiKeyTest;
+        
     //     PaymentServiceImpl paymentService = new PaymentServiceImpl();
 
     //     ProductBasket product = new ProductBasket();
@@ -280,13 +272,47 @@ public class PaymentResourceTest {
 
     //     List<ProductBasket> productList = new ArrayList<>();
     //     productList.add(product);
-        
     //     Basket basket = new Basket();
     //     basket.setProducts(productList);
 
     //     JsonObject result = paymentService.createCheckoutSession(basket);
         
     //     assertNotNull(result);
+    // }
+
+    // @Test
+    // void testCheckoutSession() throws StripeException {
+
+    //     PaymentService paymentServiceMock = Mockito.mock(PaymentService.class);
+
+    //     PaymentRestService paymentRestService = new PaymentRestService();
+        
+    //     Basket basket = new Basket();
+    //     JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
+    //     jsonObjectBuilder.add("amount",260.0);
+    //     jsonObjectBuilder.add("url", "https://checkout.stripe.com/c/pay/cs_test_b1AQ7alb3RtHZkq8A5aTkjOXbBeg2C7sMP71hPk5ALrUX832VBKujYi1Xe#fidkdWxOYHwnPyd1blpxYHZxWjA0S0NGXERBYm1JRFc8aklIX3VOQmMzalR2UVM2U2ZhN2JINkNASGREPHdJUjV0ajJxZ1VHcz0yUVRVSTA1S0tLN0F2VjJcQURwdXZHUHJpX3V0cVdmQTJTNTVrQ113YzA1MicpJ2N3amhWYHdzYHcnP3F3cGApJ2lkfGpwcVF8dWAnPydocGlxbFpscWBoJyknYGtkZ2lgVWlkZmBtamlhYHd2Jz9xd3BgeCUl");
+
+    //     JsonObject sessionJson = jsonObjectBuilder.build();
+
+    //     Mockito.when(paymentServiceMock.createCheckoutSession(any(Basket.class))).thenReturn(sessionJson);
+
+    //     Response response = paymentRestService.checkoutSession(basket);
+
+    //     assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+    // }
+
+    // @Test
+    // void testCheckoutSession_NullSession() throws StripeException {
+
+    //     PaymentService paymentServiceMock = Mockito.mock(PaymentService.class);
+
+    //     PaymentRestService paymentRestService = new PaymentRestService();
+
+    //     Mockito.when(paymentServiceMock.createCheckoutSession(any(Basket.class))).thenReturn(null);
+
+    //     Response response = paymentRestService.checkoutSession(new Basket());
+
+    //     assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
     // }
 
 /* Test PaymentRestService */
@@ -419,77 +445,4 @@ public class PaymentResourceTest {
             .then()
             .statusCode(Response.Status.NOT_FOUND.getStatusCode());
     }
-
-    // @Test
-    // void testCheckoutSession() throws StripeException {
-
-    //     PaymentService paymentServiceMock = Mockito.mock(PaymentService.class);
-
-    //     PaymentRestService paymentRestService = new PaymentRestService();
-        
-    //     Basket basket = new Basket();
-    //     JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
-    //     jsonObjectBuilder.add("amount",260.0);
-    //     jsonObjectBuilder.add("url", "https://checkout.stripe.com/c/pay/cs_test_b1AQ7alb3RtHZkq8A5aTkjOXbBeg2C7sMP71hPk5ALrUX832VBKujYi1Xe#fidkdWxOYHwnPyd1blpxYHZxWjA0S0NGXERBYm1JRFc8aklIX3VOQmMzalR2UVM2U2ZhN2JINkNASGREPHdJUjV0ajJxZ1VHcz0yUVRVSTA1S0tLN0F2VjJcQURwdXZHUHJpX3V0cVdmQTJTNTVrQ113YzA1MicpJ2N3amhWYHdzYHcnP3F3cGApJ2lkfGpwcVF8dWAnPydocGlxbFpscWBoJyknYGtkZ2lgVWlkZmBtamlhYHd2Jz9xd3BgeCUl");
-
-    //     JsonObject sessionJson = jsonObjectBuilder.build();
-
-    //     Mockito.when(paymentServiceMock.createCheckoutSession(any(Basket.class))).thenReturn(sessionJson);
-
-    //     Response response = paymentRestService.checkoutSession(basket);
-
-    //     assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
-    // }
-
-//     @Test
-//     void testCheckoutSession_NullSession() throws StripeException {
-
-//         PaymentService paymentServiceMock = Mockito.mock(PaymentService.class);
-
-//         PaymentRestService paymentRestService = new PaymentRestService();
-
-//         Mockito.when(paymentServiceMock.createCheckoutSession(any(Basket.class))).thenReturn(null);
-
-//         Response response = paymentRestService.checkoutSession(new Basket());
-
-//         assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
-//     }
-
-    // @Test
-    // void testUpdateAmount() {
-    //     JsonObject requestJson = Json.createObjectBuilder()
-    //             .add("amount", 100)
-    //             .build();
-
-    //     PaymentRestService paymentRestService = new PaymentRestService();
-
-    //     Response response = paymentRestService.updateAmount(requestJson);
-
-    //     assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
-    // }
-
-
-    // @Test
-    // void testPersistenceAndRetrieval() {
-    //     // Create a new Profit object
-    //     Profit profit = new Profit();
-    //     profit.setAmount(100.0);
-    //     profit.setCreated_at(LocalDate.now());
-
-    //     // Persist the Profit object to the database
-    //     profit.persist();
-
-    //     // Retrieve the persisted Profit object by its ID
-    //     Profit retrievedProfit = Profit.findById(profit.id);
-
-    //     // Assert that the retrieved Profit object is not null
-    //     assertNotNull(retrievedProfit);
-
-    //     // Assert that the retrieved Profit object has the same amount and created_at values
-    //     assertEquals(profit.getAmount(), retrievedProfit.getAmount());
-    //     assertEquals(profit.getCreated_at(), retrievedProfit.getCreated_at());
-
-    //     // Assert that the profit object is persistent
-    //     
-    //}
 }
